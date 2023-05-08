9438c48c8c74a80bb39d2e509184555d7e4fd871
Harshit Kumar <bkharshitbk@gmail.com>
2023-05-08 11:56:39 +0530

    tablist link to navlink

--- src/components/OfferingTab/OfferingTab.js
+++ src/components/OfferingTab/OfferingTab.js
@@ -1,27 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { tabLinkStyle } from "../../utils/css/OfferingTab.css";
 const OfferingTab = () => {
   return (
     <>
-      <Link to="/online-order" className="tab-link">
+      <NavLink
+        to="/online-order"
+        replace
+        className="tab-link"
+        activeClassName="active"
+      >
         <div style={tabLinkStyle} className="tab-link-delivery ">
           <h2 className="tab-link-title">Delivery</h2>
           <hr className="hr-below-tablink" />
         </div>
-      </Link>
+      </NavLink>
 
-      <Link to="/dining" className="tab-link">
+      <NavLink
+        to="/dining"
+        replace
+        className="tab-link"
+        activeClassName="active"
+      >
         <div style={tabLinkStyle} className="tab-link-dining-out">
           <h2 className="tab-link-title">Dining Out</h2>
           <hr className="hr-below-tablink" />
         </div>
-      </Link>
-      <Link to="/night-life-and-clubs" className="tab-link">
+      </NavLink>
+
+      <NavLink
+        to="/night-life-and-clubs"
+        replace
+        className="tab-link"
+        activeClassName="active"
+      >
         <div style={tabLinkStyle} className="tab-link-nightlife">
           <h2 className="tab-link-title">Nightlife</h2>
           <hr className="hr-below-tablink" />
         </div>
-      </Link>
+      </NavLink>
     </>
   );
 };
