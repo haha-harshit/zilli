203faf74350c4457c8b19fa4b49af070d53bec6c
Harshit Kumar <bkharshitbk@gmail.com>
2023-06-02 00:23:26 +0530

    margin to res layout

--- src/components/Cards/ResCards/ResCard.js
+++ src/components/Cards/ResCards/ResCard.js
@@ -6,20 +6,22 @@ const ResCard = (props) => {
   return (
     <>
       <div className="res-card-layout">
-        <Link to={"/" + resData.data.name}>
-          <div className="res-card-img-main">
-            <img
-              className="res-img"
-              src={
-                "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,g_auto,w_508,h_320,c_fill/" +
-                resData.data.cloudinaryImageId
-              }
-              alt="res-logo"
-            />
-          </div>
+        <div className="res-contents">
+          <Link to={"/" + resData.data.name}>
+            <div className="res-card-img-main">
+              <img
+                className="res-img"
+                src={
+                  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,g_auto,w_508,h_320,c_fill/" +
+                  resData.data.cloudinaryImageId
+                }
+                alt="res-logo"
+              />
+            </div>
 
-          <h3 className="res-card-img">{resData.data.name}</h3>
-        </Link>
+            <h3 className="res-card-title">{resData.data.name}</h3>
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/components/OrderOnline/OrderOnline.js
+++ src/components/OrderOnline/OrderOnline.js
@@ -7,12 +7,13 @@ import offerCardDetails from "../../utils/mockData";
 // style imports
 import { tabListStyle } from "../../utils/css/OfferingTab.css";
 import { OrderOnlineStyle } from "../../utils/css/OrderOnline.css";
+import { resStyle } from "../../utils/css/resCard.css";
 import resList from "../../utils/resList";
 
 const OrderOnline = () => {
   return (
     <>
-      {console.log(resList)}
+      {/* {console.log(resList)} */}
       <div className="section">
         <div style={tabListStyle} className="offering-tab-list">
           <OfferingTab />
@@ -28,7 +29,7 @@ const OrderOnline = () => {
           <h1>Order food online in NIT - 3, Faridabad, India</h1>
         </div>
 
-        <div style={OrderOnlineStyle} className="res-container">
+        <div style={resStyle} className="res-container">
           {resList.map((restaurant) => (
             <ResCard resData={restaurant} key={restaurant.data.id} />
           ))}
