1b0e896278d9a1c96f4198772d7634b2bb6bd326
harshit <bkharshitbk@gmail.com>
2023-09-11 22:15:36 +0530

    tailwind for offercards at landing

--- src/components/Cards/OfferingCards/OfferCard.js
+++ src/components/Cards/OfferingCards/OfferCard.js
@@ -5,14 +5,14 @@ const OfferCard = (props) => {
   const { url, heading, info, type } = details;
   return (
     <>
-      <div className="offer-card-layout">
-        <Link to={type}>
-          <div className="offer-card-img-main">
-            <img className="offer-card-img" src={url} alt="offer-card-image" />
+      <div className="offer-card-layout h-[15rem] w-[21rem] border-[0.01rem] border-solid border-[rgb(227, 227, 227)] rounded-[0.7rem] m-[2%]">
+        <Link to={type} className="decoration-[none]">
+          <div className="offer-card-img-main h-[65%]">
+            <img className="offer-card-img h-full w-full object-cover rounded-t-xl rounded-tr-xl" src={url} alt="offer-card-image" />
           </div>
-          <div className="offer-card-info">
-            <p className="offer-card-title">{heading}</p>
-            <p className="offer-card-sub-title">{info}</p>
+          <div className="offer-card-info p-[0.7rem]">
+            <p className="offer-card-title text-xl font-medium text-[rgb(54, 54, 54)]">{heading}</p>
+            <p className="offer-card-sub-title font-normal text-[rgb(79, 79, 79)]">{info}</p>
           </div>
         </Link>
       </div>
