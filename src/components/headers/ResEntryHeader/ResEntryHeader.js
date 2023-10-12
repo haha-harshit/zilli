f6f717d857d6755c24ec463360ce964a3aea44e8
harshit <bkharshitbk@gmail.com>
2023-10-12 23:51:16 +0530

    back button mob

--- src/components/headers/ResEntryHeader/ResEntryHeader.js
+++ src/components/headers/ResEntryHeader/ResEntryHeader.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom"
 
 const ResEntryHeader = () => {
     return(
         <>
         <div className="res-entry-header flex flex-row justify-between w-[85vw] m-auto mt-[2rem] sm:hidden">
             <div className="back_WRAPPER">
+                <Link to={"javascript:history.back()"}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                 </svg>
+                </Link>
             </div>
 
             <div className="flex flex-row justify-evenly">
