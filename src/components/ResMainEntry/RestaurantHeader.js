7d10c5de523dba0d81481929868592f4c213f38a
harshit <bkharshitbk@gmail.com>
2023-10-13 14:51:07 +0530

    distance_wrapper_mob

--- src/components/ResMainEntry/RestaurantHeader.js
+++ src/components/ResMainEntry/RestaurantHeader.js
@@ -18,20 +18,20 @@ const RestaurantHeader = (props) => {
                         {cuisines.join(", ")}
                     </div>
 
-                    <div className="flex items-center">
+                    <div className="ratingCount_wrapper_mob flex items-center mb-[0.5rem]">
                     <div className="sm:hidden flex items-center justify-around px-[0.3rem] py-1 bg-green-700 text-my-white font-semibold rounded-md text-xs">{resMainInfo.avgRating}
                     
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3 ml-[0.1rem]">
                     <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
                     </svg>
                     </div>
-                    <div className="RestaurantRatingCount font-bold text-[0.8rem] ml-[0.25rem] text-gray-800 underline">
+                    <div className="RestaurantRatingCount font-bold text-[0.8rem] ml-[0.25rem] text-gray-800 sm:hidden">
                         <div className="py-[0.2rem]">{resMainInfo.totalRatingsString} ratings</div>
                     </div>
                     </div>
 
-                    <div className="RestaurantAddessDistance_wrapper">
-                        {resMainInfo.areaName}, {resMainInfo.sla.lastMileTravel} km
+                    <div className="RestaurantAddressDistance_mob_wrapper p-[0.5rem] rounded-[2rem] bg-gray-100 sm:p-0 sm:rounded-none sm:bg-inherit">
+                    {resMainInfo.sla.lastMileTravel} km • {resMainInfo.areaName}
                     </div>
                     
                 </div>
@@ -43,9 +43,9 @@ const RestaurantHeader = (props) => {
                 </div>
             </div>
             
-            <hr className="RestaurantDottedSeparator"/>
+            <hr className="RestaurantDottedSeparator hidden sm:inline-block"/>
 
-            <div className="RestaurantTimeCost_wrapper">
+            <div className="RestaurantTimeCost_wrapper hidden sm:inline-block">
                 <ul>
                     <li className="RestaurantTimeCost_item">
                         {resMainInfo.sla.deliveryTime} MINS
